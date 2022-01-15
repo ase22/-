0f0866e4b4b764d14bbc62e652e4b3515c0dbf30
AhnSangEon <stdleaf@gmail.com>
2022-01-15 09:38:25 +0900

    가장 긴 동일 값의 경로

--- 14장_트리/43. 이진 트리의 직경.py
+++ 14장_트리/43. 이진 트리의 직경.py
@@ -73,13 +73,13 @@ s = Solution()
 class Solution2:
   # 중첩함수에서 부모 함수의 변수를 재할당하면 부모에 있는 변수에 할당되는게 아니라 중첩 함수 내에 지역변수가 생기고 이것에 할당을 하게 된다.
   # 따라서 바깥에 클래스 변수로 선언해서 자유롭게 할당할 수 있게 해준다.
-  longest: int = 0 
+  longest: int = 0
 
   def diameterOfBinaryTree(self, root: TreeNode) -> int:
     def dfs(node: TreeNode) -> int:
       if not node:
         return -1
-      
+
       # 왼쪽, 오른쪽의 각 리프 노드까지 탐색
       left = dfs(node.left)
       right = dfs(node.right)
@@ -89,11 +89,40 @@ class Solution2:
 
       # 상태값 리턴
       return max(left, right) + 1
-    
+
     dfs(root)
     return self.longest
 
 S = Solution2()
 
 print(S.diameterOfBinaryTree(root))
+
+
+# dfs 풀이 연습
+# 푸는 방법
+# 1. dfs로 맨 밑까지 탐색한다고 생각한다.
+# 2. 탐색 결과 무엇을 리턴해야 할 지 생각한다. 리프노드와 인자로 받은 노드까지의 거리를 리턴한다.
+# 3. 여기서는 리프 노드를 기준으로 깊이가 -1인 곳 까지 가기 때문에 max(left, right) + 1을 리턴한다. 이러면 리프 노드에서의 dfs 값은 0이 된다.
+# 4. 최대 길이는 루트 노드의 두 자식 노드의 리프 노드까지의 거리 + 2이기 때문에 left + right + 2가 최대 길이이다.
+
+class Practice:
+  longest: int = 0
+
+  def practice(self, root):
+    def dfs(node: TreeNode):
+      if not node:
+        return -1
+
+      left = dfs(node.left)
+      right = dfs(node.right)
+
+      self.longest = max(self.longest, left + right + 2)
+
+      return max(left, right) + 1
+
+    dfs(root)
+
+    return self.longest
+
+
 
---
+++ 14장_트리/44. 가장 긴 동일 값의 경로.py
@@ -0,0 +1,53 @@
+# 동일한 값을 지닌 가장 긴 경로를 찾아라.
+
+class TreeNode:
+  def __init__(self, val = 0):
+    self.value = val
+    self.left = None
+    self.right = None
+
+# 내 풀이
+# 이전 문제의 가장 긴 경로 구하는 문제를 이용해서 부모 노드와 자식 노드의 값을 비교한다.
+# 3가지 경우로 나눠서 자식 노드 둘 다 부모와 같은 경우, 왼쪽만 같은 경우, 오른쪽만 같은 경우 이렇게 나누었다.
+class Solution:
+  longest: int = 0
+  # 내 풀이
+
+  def longestUnivaluePath(self, root) -> int:
+    def dfs(node: TreeNode) -> int:
+      if not node:
+        return -1
+
+      left = dfs(node.left)
+      right = dfs(node.right)
+
+      if node.left and node.right and node.value == node.left.value and node.value != node.right.value:
+        self.longest = max(self.longest, left + 1)
+
+      if node.right and node.left and node.value != node.left.value and node.value == node.right.value:
+        self.longest = max(self.longest, right + 1)
+
+      if node.right and node.left and node.value == node.left.value and node.value == node.right.value:
+        self.longest = max(self.longest, left + right + 2)
+
+      return max(left, right) + 1
+
+    dfs(root)
+
+    return self.longest
+
+root = TreeNode(1)
+n2 = TreeNode(4)
+n3 = TreeNode(5)
+n4 = TreeNode(4)
+n5 = TreeNode(4)
+n6 = TreeNode(5)
+
+root.left = n2
+root.right = n3
+n2.left = n4
+n2.right = n5
+n3.right = n6
+
+s = Solution()
+print(s.longestUnivaluePath(root))
