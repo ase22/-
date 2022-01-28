5532433651388988fcd87c3093f4183132ce63e9
AhnSangEon <stdleaf@gmail.com>
2022-01-28 12:59:56 +0900

    배낭문제 js로 구현

---
+++ 21장_그리디_알고리즘/배낭문제.js
@@ -0,0 +1,32 @@
+const cargo = [
+  [4, 12],
+  [2, 1],
+  [10, 4],
+  [1, 1],
+  [2, 2]
+];
+
+const capacity = 15;
+
+function fillinBackpack(cargo, capacity) {
+  let backpackCapacity = capacity;
+  let money = 0;
+
+  const pack = cargo.map(item => [item[0] / item[1], item[0], item[1]]).sort((a, b) => b[0] - a[0]);
+
+  for (item of pack) {
+    if ((backpackCapacity - item[2]) >= 0) {
+      backpackCapacity -= item[2];
+      money += item[1];
+    }
+    else {
+      const remainingWeightProportion = backpackCapacity / item[2];
+      backpackCapacity = 0;
+      money += remainingWeightProportion * item[0];
+    }
+  }
+
+  return money;
+}
+
+console.log(fillinBackpack(cargo, capacity));
